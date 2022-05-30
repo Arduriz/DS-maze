b54a8cf00c81edd55967666e6ad649fbf44ce5ea
CHOI Hyunseo <65582244+Arduriz@users.noreply.github.com>
2022-05-31 02:32:21 +0900

    Update input.c

--- input.c
+++ input.c
@@ -2,29 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_STACK_SIZE 6
-#define MAZE_SIZE 100
+#define MAX_STACK_SIZE 7
+#define MAZE_SIZE 7
 
 int i, j = 0;//지역변수 초기화에 에러가 걸려서 아예 맨처음에 선언과 초기화
 
+typedef struct {
+    short r;
+    short c;
+}
+
 void main() {
     char maze[MAZE_SIZE][MAZE_SIZE];//파일에서 읽어들인 문자열을 저장하기 위한 배열
-    char* buf;//분할된 문자열의 포인터 값을 저장할 변수
 
     FILE* file_read = fopen("input.txt", "r");//fopen이 반환하는 파일 위치를 받을 포인터 변수 file_read 선언
 
-    while (fgets(maze, sizeof(maze), file_read) != NULL) { //fgets 함수로 파일의 마지막 문장을 읽을 때까지 실행
-        buf = strtok(maze, " ");//strtok 함수로 공백을 기준으로 문자열을 분할한 뒤 포인터 반환
-        while (buf != NULL) { //자른 문자열이 나오지 않을 때까지 반복
-            maze[i][j] = atoi(buf);//첫번째 값으로 계수, 두번째 값으로 차수가 배열에 저장
-            i++;//while문이 실행될 때마다 1 증가
-            buf = strtok(NULL, " ");//다음 문자열을 잘라서 포인터를 반환
-            if (buf == "\r\n") {
-                i = 0;
-                j++;
-            }
+    for (int i = 0; i < MAZE_SIZE-1; i++){
+        for (int j = 0; j < MAZE_SIZE-1; j++) {
+            fscanf(file_read, "%s", &maze[i][j]);
         }
     }
-    
+
+    file_read = fopen("output.txt", "w");
+    for (int r = 0; r < MAZE_SIZE-1; r++)
+    {
+        for (int c = 0; c < MAZE_SIZE-1; c++)
+        {
+            if (maze[r][c] == '1')
+                fprintf(file_read, "1");
+            else if (maze[r][c] == '0')
+                fprintf(file_read, "0");
+            else if (maze[r][c] == 'E')
+                fprintf(file_read, "E");
+            else if (maze[r][c] == '.')
+                fprintf(file_read, ".");
+            else if (maze[r][c] == 'X')
+                fprintf(file_read, "X");
+            else
+                fprintf(file_read, "N");
+        }
+        fprintf(file_read, "\n");
+    }
     return 0;
 }
